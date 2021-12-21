24d210d7379611f841ad781850147b0217dc2004
Kyle Wayman <kwayman@gmail.com>
2021-12-21 10:54:11 -0800

    Update.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+class App extends Component {
+  componentDidMount() {
+    const api = 'https://am7aydgcsg.execute-api.us-east-2.amazonaws.com/dev';
+    const data = { "firstName" : "Spuds", "Mackenzie" : "Hunt" };
+    axios
+      .post(api, data)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+  render() {
+    return (
+      <div>Medium Tutorial</div>
+    );
+  }
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +44,6 @@ function App() {
     </div>
   );
 }
+*/
 
 export default App;
